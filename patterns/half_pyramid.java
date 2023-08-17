91a8e167f13cc506bbc8f882ef7fff995489ab7d
Ashish patil <47731642+ashishpatil7507@users.noreply.github.com>
2023-08-17 23:57:57 +0530

    Add files via upload

---
+++ patterns/half_pyramid.java
@@ -0,0 +1,10 @@
+public class half_pyramid {
+    public static void main(String[] args) {
+        // int n=5;
+        for (int line=1; line<=5; line++ ){
+            for(int number = 1; number<=line; number ++ ){
+                System.out.print(number);
+            }System.out.println();
+        }
+    }
+}
