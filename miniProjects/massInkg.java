37f02aa14edb5d79f4ff6486f76a4a9d83d7fda0
Ashish patil <47731642+ashishpatil7507@users.noreply.github.com>
2023-08-17 23:57:26 +0530

    Add files via upload

---
+++ miniProjects/massInkg.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class massInkg {
+  
+    public static void main(String[] args) {
+        int mass = 5;
+        int velocity = 12;
+        int momentum = mass * velocity * velocity;
+
+        System.out.println(momentum);
+    }
+
+}
